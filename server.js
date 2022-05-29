6c3465d4daa6086e151279296fd4d15119be2d02
thomasdussart <thomas.dussart@gmail.com>
2022-05-29 22:27:55 +0200

    add jwt auth for login / register

--- server.js
+++ server.js
@@ -125,8 +125,15 @@ app.post("/login", async (req, res) => {
 });
 
 // start server
-const port =
-  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-app.listen(port, () => {
-  console.log("Server listening on port " + port);
+// const port =
+//   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+// app.listen(port, () => {
+//   console.log("Server listening on port " + port);
+// });
+
+//heroku
+const server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+const server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, () => {
+  console.log("Listening on port %d", server_port);
 });
